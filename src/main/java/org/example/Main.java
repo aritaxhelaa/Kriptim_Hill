903cb9e62afa88eccb549078b8b0dfd9884fcce9
Arita <arita.xhela@student.uni-pr.edu>
2025-05-11 15:21:46 +0200

    EncryptBlock

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,6 +3,22 @@ package org.example;
 
 public class Main {
 
+    static String encryptBlock(int[][] keyMatrix, int[] block) {
+        int n = block.length;
+        int[] result = new int[n];
+        for (int i = 0; i < n; i++) {
+            result[i] = 0;
+            for (int j = 0; j < n; j++) {
+                result[i] += keyMatrix[i][j] * block[j];
+            }
+            result[i] = result[i] % mod;
+        }
+        StringBuilder sb = new StringBuilder();
+        for (int val : result) {
+            sb.append(numToChar(val));
+        }
+        return sb.toString();
+    }
 
     static String encrypt(String input, int[][] keyMatrix, int n) {
         StringBuilder encrypted = new StringBuilder();
