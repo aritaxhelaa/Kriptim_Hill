345c41f277d9b902e3a20ec9be42b7b1f158c04e
BahrieVeseli <bahrie.veseli@student.uni-pr.edu>
2025-05-11 15:32:14 +0200

    added main

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,8 +1,10 @@
 package org.example;
 
 
+import java.util.Scanner;
+
 public class Main {
-static int mod = 26;
+    static int mod = 26;
 
 
     static int charToNum(char c) {
@@ -12,6 +14,7 @@ static int mod = 26;
     static char numToChar(int n) {
         return (char) (n + 'A');
     }
+
     static String encryptBlock(int[][] keyMatrix, int[] block) {
         int n = block.length;
         int[] result = new int[n];
@@ -47,12 +50,8 @@ static int mod = 26;
         return encrypted.toString();
     }
 
-    public static void main(String[] args) {
 
-        }
-    }
-}
-static int[][] readMatrix(Scanner sc, int n) {
+    static int[][] readMatrix(Scanner sc, int n) {
         int[][] matrix = new int[n][n];
         System.out.println("Fut elementët e matrices (" + n + "x" + n + "):");
         for (int i = 0; i < n; i++) {
@@ -62,3 +61,27 @@ static int[][] readMatrix(Scanner sc, int n) {
         }
         return matrix;
     }
+    public static void main(String [] args){
+        Scanner sc = new Scanner(System.in);
+
+        int n;
+
+        do {
+            System.out.print("Zgjidh madhësinë e matrices (2 - 4): ");
+            n = sc.nextInt();
+            if (n < 2 || n > 4) {
+                System.out.println("Gabim! Ju lutem shtypni një numër nga 2 deri në 4.");
+            }
+        } while (n < 2 || n > 4);
+
+        int[][] keyMatrix = readMatrix(sc, n);
+        sc.nextLine();
+
+        System.out.print("Shkruaj tekstin për kriptim: ");
+        String input = sc.nextLine();
+
+        String encrypted = encrypt(input, keyMatrix, n);
+        System.out.println("Teksti i koduar: " + encrypted);
+    }
+
+}
